f9d7157133de1a6db8528c6017670969744c03a3
Suman Sourabh <suman.sourabh@ansrsource.com>
2024-02-04 01:10:12 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URI,
+  })
+);
 
 // import routes
 const authRoutes = require("./routes/authRoutes");
